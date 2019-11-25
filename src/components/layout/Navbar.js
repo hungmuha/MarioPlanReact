9fecc1b8c5a6e7c6f92b71e1ececf6b870142453
hungmuha <hung.soboss@gmail.com>
2019-11-24 21:30:30 -0700

    created Dashboard and Navbar components

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
-import SignedinLink from './SignedinLinks'
+import SignedInLinks from './SignedInLinks'
+import SignedOutLinks from './SignedOutLinks'
 
 const Navbar = () => {
     return(
         <nav className="nav-wrapper grey darken-3">
             <div className="container">
                 <Link to='/' className="brand-logo">MarioPlan</Link>
-                <SignedinLink></SignedinLink>
+                <SignedInLinks></SignedInLinks>
+                <SignedOutLinks></SignedOutLinks>
             </div>
         </nav>
     )
