7b2514d0a4ecfbc6d56f8410931cad80a0999e87
hungmuha <hung.soboss@gmail.com>
2019-11-30 20:01:54 -0700

    fixed the version fof rof  react-redux-firebase to establish connection with firebase via redux

--- src/index.js
+++ src/index.js
@@ -3,16 +3,31 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import {createStore, applyMiddleware} from 'redux';
+import {createStore, applyMiddleware, compose} from 'redux';
 //import createStore for storing state, applyMiddleware to apply thunk 
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
+import {reduxFirestore, getFirestore} from 'redux-firestore';
+import {reactReduxFirebase, getFirebase} from 'react-redux-firebase';
+import fbConfig from './config/fbConfig';
 import rootReducer from './store/reducers/rootReducer'
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const rrfConfig = { userProfile: 'users' } // react-redux-firebase config
+const store = createStore(rootReducer, 
+    compose(
+        //using compose to combine store-enhancers, in order to bind firestore with redux via config
+        //so when we are using getFirebase and getFirestore, redux know which project we are connecting to
+        applyMiddleware(thunk.withExtraArgument({getFirebase,getFirestore})),
+        reactReduxFirebase(fbConfig,rrfConfig),
+        reduxFirestore(fbConfig) 
+    )
+);
 //reducer being passed to the store here 
 //redux thunk is middle and need to be applied here
 //with thunk we can have action-creator interact with database
+//pass in get firebase and firestore to think.withExtraArgument to use the 2 functions that allow Redux
+//to interact with firestore API and firebase, since thunk allow action to return a function 
+
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -3,9 +3,21 @@ export const createProject = (project) =>{
     //     type:'ADD-PROJECT',
     //     project: project
     // }
-    //above is how action usually done but with thunk we will return a function which allow us to make asyn call
-    return(dispatch,getState) => {
+    //above is how action usually done but with 'thunk' we will return a function which allow us to make asyn call
+    return(dispatch,getState, {getFirebase, getFirestore}) => {
         //make async call to database 
-        dispatch({type:'CREATE_PROJECT', project});
+        const firestore = getFirestore();
+        firestore.collection('projects').add({
+            ...project,
+            authorFirstName:'Net',
+            authorLastName:'Hung',
+            authorId: 12345,
+            createdAt: new Date()
+        }).then(() => {
+            dispatch({type:'CREATE_PROJECT', project});
+        }).catch((err) => {
+            dispatch({ type:'CREATE_PROJECT_ERROR'}, err);
+        });
+        
     }
 }
